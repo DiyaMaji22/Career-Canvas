7c39d4cd2f42726154dc9685d5858e38c1aac064
Ansuman Pal <135434905+Blank-16@users.noreply.github.com>
2025-02-14 19:40:41 +0530

    ...

--- Career-Canvas/src/App.jsx
+++ Career-Canvas/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import Nav from './Nav.jsx'
+
 
 function App() {
-
+const [counter, setCounter] = useState(0);
   return (
     <>
-      <div className='w-full min-h-screen'></div>
+      <div className='w-full min-h-screen'>
+      <Nav />
+
+      </div>
     </>
   )
 }
