9670a2b40d4915d0fd3c677318bc3419782704f3
Diya Maji <diyamaji22@gmail.com>
2025-02-14 22:29:53 +0530

    ..

--- Career-Canvas/src/App.jsx
+++ Career-Canvas/src/App.jsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
-<<<<<<< HEAD
 import Body from "./Body.jsx"
-=======
-import Nav from './Nav.jsx'
-
-
->>>>>>> 7c39d4cd2f42726154dc9685d5858e38c1aac064
+import Nav from "./Nav.jsx"
 function App() {
 const [counter, setCounter] = useState(0);
   return (
     <>
       <div className='w-full min-h-screen'>
-<<<<<<< HEAD
-        <Body />
-=======
       <Nav />
-
->>>>>>> 7c39d4cd2f42726154dc9685d5858e38c1aac064
+      <Body />
       </div>
     </>
   )
--- Career-Canvas/src/Body.jsx
+++ Career-Canvas/src/Body.jsx
@@ -1,9 +1,13 @@
+import background from "./assets/background.jpg"
+
 function Body()
 {
     return(
     <>
-        <div class="bg-yellow-200 w-max h-screen">
-
+        <div class="bg-yellow-100 m-8 size-auto">
+            <img src={background} alt="background" />
+            
+            
         </div>
     </>
     )
